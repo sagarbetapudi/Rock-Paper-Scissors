97dca524cafd3d148518dffe14c9e2e9b8d8ab7d
Sagar Betapudi <73014005+sagarbetapudi@users.noreply.github.com>
2024-10-29 10:32:11 +0530

    Update Rock_Paper_Scissors.py

--- Rock_Paper_Scissors.py
+++ Rock_Paper_Scissors.py
@@ -1,6 +1,6 @@
 import random
 
-print('Winning rules of the game ROCK PAPER SCISSORS are:\n'
+print("Winning rules of the game ROCK PAPER SCISSORS are:\n"
       + "Rock vs Paper -> Paper wins \n"
       + "Rock vs Scissors -> Rock wins \n"
       + "Paper vs Scissors -> Scissors wins \n")
@@ -12,41 +12,41 @@ while True:
     choice = int(input("Enter your choice: "))
 
     while choice > 3 or choice < 1:
-        choice = int(input('Enter a valid choice please : '))
+        choice = int(input("Enter a valid choice please : "))
 
     if choice == 1:
-        choice_name = 'Rock'
+        choice_name = "Rock"
     elif choice == 2:
-        choice_name = 'Paper'
+        choice_name = "Paper"
     else:
-        choice_name = 'Scissors'
+        choice_name = "Scissors"
 
-    print('User choice is:', choice_name)
-    print("Now it's Computer's Turn...")
+    print("User choice is:", choice_name)
+    print("Now it"s Computer"s Turn...")
 
     comp_choice = random.randint(1, 3)
 
     if comp_choice == 1:
-        comp_choice_name = 'Rock'
+        comp_choice_name = "Rock"
     elif comp_choice == 2:
-        comp_choice_name = 'Paper'
+        comp_choice_name = "Paper"
     else:
-        comp_choice_name = 'Scissors'
+        comp_choice_name = "Scissors"
 
     print("Computer choice is:", comp_choice_name)
-    print(choice_name, 'vs', comp_choice_name)
+    print(choice_name, "vs", comp_choice_name)
 
     if choice == comp_choice:
         result = "DRAW"
     elif (choice == 1 and comp_choice == 2) or (comp_choice == 1 and choice == 2):
-        result = 'Paper'
+        result = "Paper"
     elif (choice == 1 and comp_choice == 3) or (comp_choice == 1 and choice == 3):
-        result = 'Rock'
+        result = "Rock"
     elif (choice == 2 and comp_choice == 3) or (comp_choice == 2 and choice == 3):
-        result = 'Scissors'
+        result = "Scissors"
 
     if result == "DRAW":
-        print("<== It's a tie! ==>")
+        print("<== It"s a tie! ==>")
     elif result == choice_name:
         print("<== User wins! ==>")
     else:
@@ -54,7 +54,7 @@ while True:
 
     print("Do you want to play again? (Y/N)")
     ans = input().lower()
-    if ans == 'n':
+    if ans == "n":
         break
 
 print("Thanks for playing!")
